f3995f39ac71c9f94527018fb4350b9098911283
Mansithakkar06 <mansithakkar203@gmail.com>
2025-11-11 17:50:53 +0530

    added user and video model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv'
 import dbconnection from './db/connection.js';
+import { app } from './app.js';
 
 dotenv.config({
     path:"./env"
 })
 dbconnection()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("server is listening on port: ",process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.log("ERROR: CONNECTION TO DATABASE FAILLED!!",error)
+})
