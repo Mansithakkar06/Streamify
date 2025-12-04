2ddf610c25ac67728630ed6b5f6ec3135473f6b5
Mansithakkar06 <mansithakkar203@gmail.com>
2025-12-04 21:00:10 +0530

    some changes in sidebar paths

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,15 +9,15 @@ function Sidebar() {
     <div className='p-2 border-r border-r-white flex flex-col justify-between w-64 min-h-screen position-sticky top-0'>
       <div className='p-2 space-y-2'>
         <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faHome} name="Home" /></NavLink>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faThumbsUp} name="Liked Videos" /></NavLink>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faHistory} name="History" /></NavLink>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faVideoCamera} name="My Content" /></NavLink>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faFolder} name="Collections" /></NavLink>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faUserCheck} name="Subscribers" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faThumbsUp} name="Liked Videos" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faHistory} name="History" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faVideoCamera} name="My Content" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faFolder} name="Collections" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faUserCheck} name="Subscribers" /></NavLink>
       </div>
       <div className='p-2 space-y-2'>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faCircleQuestion} name="Support" /></NavLink>
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faGear} name="Settings" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faCircleQuestion} name="Support" /></NavLink>
+        <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faGear} name="Settings" /></NavLink>
       </div>
     </div>
   )
