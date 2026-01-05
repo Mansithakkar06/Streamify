3b3178d40dfd263e153e35931885ce160c9527c4
Mansithakkar06 <mansithakkar203@gmail.com>
2026-01-05 20:08:06 +0530

    feat: add Modal component and update Sidebar to handle optional chaining for username

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -13,9 +13,9 @@ function Sidebar() {
         <NavLink to="/" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faHome} name="Home" /></NavLink>
         <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faThumbsUp} name="Liked Videos" /></NavLink>
         <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faHistory} name="History" /></NavLink>
-        <NavLink to={`/channel/${user.username}/Videos`} className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faVideoCamera} name="My Content" /></NavLink>
-        <NavLink to={`/channel/${user.username}/Playlist`} className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faFolder} name="Collections" /></NavLink>
-        <NavLink to={`/channel/${user.username}/Subscribers`} className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faUserCheck} name="Subscribers" /></NavLink>
+        <NavLink to={`/channel/${user?.username}/Videos`} className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faVideoCamera} name="My Content" /></NavLink>
+        <NavLink to={`/channel/${user?.username}/Playlist`} className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faFolder} name="Collections" /></NavLink>
+        <NavLink to={`/channel/${user?.username}/Subscribers`} className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faUserCheck} name="Subscribers" /></NavLink>
       </div>
       <div className='p-2 space-y-2'>
         {/* <NavLink to="*" className={({ isActive }) => isActive ? "bg-[#ae7aff] text-black block" : ""}><SidebarItem icon={faCircleQuestion} name="Support" /></NavLink> */}
